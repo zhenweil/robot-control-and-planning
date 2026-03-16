a8612d3ce09fb3eb46f9c6665435f38ca166ee23
zhenweil <zhenweil.career@gmail.com>
2026-03-16 00:09:16 -0700

    Added a simple launch file to publish basic robotic arm information

---
+++ src/panda_arm_moveit/launch/panda_arm.launch.py
@@ -0,0 +1,43 @@
+# launch/display.launch.py
+from launch_ros.actions import Node
+from launch import LaunchDescription
+from launch.substitutions import FindExecutable
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.parameter_descriptions import ParameterValue
+from launch.substitutions import Command, PathJoinSubstitution
+
+def generate_launch_description():
+    pkg_share = FindPackageShare("panda_arm_moveit")
+
+    xacro_file = PathJoinSubstitution([
+        pkg_share, "config", "panda.urdf.xacro"
+    ])
+
+    robot_description = {
+        "robot_description": ParameterValue(
+            Command([FindExecutable(name="xacro"), " ", xacro_file]),
+            value_type=str,
+        )
+    }
+
+    return LaunchDescription([
+        Node(
+            package="joint_state_publisher_gui",
+            executable="joint_state_publisher_gui",
+            name="joint_state_publisher_gui",
+            output="screen",
+        ),
+        Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            name="robot_state_publisher",
+            output="screen",
+            parameters=[robot_description],
+        ),
+        Node(
+            package="rviz2",
+            executable="rviz2",
+            name="rviz2",
+            output="screen",
+        ),
+    ])
