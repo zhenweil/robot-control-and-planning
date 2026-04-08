c973bf56ad5fdad1d60262a85281c1cd317ead5d
zhenweil <zhenweil.career@gmail.com>
2026-04-07 21:58:37 -0700

    Add a function to return to initial state

--- src/panda_arm_control/src/panda_arm_control.cpp
+++ src/panda_arm_control/src/panda_arm_control.cpp
@@ -36,19 +36,32 @@ class WaypointFollower : public rclcpp::Node
 			RCLCPP_INFO(this->get_logger(), "Subscriber created");
 		}
 	private:
-		std::vector<double> initial_joints = {0.0, 0.8862750036938374, 1.239396656735072, 0.0, 0.39818127891729277, 2.1916917216258462, 0.44473827755952144, 1.0247297228858394, 0.5068586069614855};
+		std::vector<double> initial_joints = {0.0, -0.8745, 0.0, -2.356, 0.0, 1.571, 0.785};
 
 		std::shared_ptr<moveit::planning_interface::MoveGroupInterface> move_group;
 		rclcpp::Subscription<geometry_msgs::msg::PoseArray>::SharedPtr sub;
+
+		void return_to_initial_pose()
+		{
+			moveit::planning_interface::MoveGroupInterface::Plan return_plan;
+			// this->move_group->setStartStateToCurrentState();
+			this->move_group->setJointValueTarget(this->initial_joints);
+			bool ok = static_cast<bool>(this->move_group->plan(return_plan));
+
+			if (ok)
+			{
+				RCLCPP_INFO(this->get_logger(), "Returning to initial state");
+				this->move_group->execute(return_plan);
+			}
+			else
+			{
+				RCLCPP_ERROR(this->get_logger(), "Failed to plan back to initial joint state");
+			}
+		}
+
 		void waypointCallback(const geometry_msgs::msg::PoseArray::SharedPtr msg)
 		{
-			this->move_group->setStartStateToCurrentState();
-			this->move_group->setJointValueTarget(this->initial_joints);
-			moveit::planning_interface::MoveGroupInterface::Plan start_pose_plan;
-			if (static_cast<bool>(this->move_group->plan(start_pose_plan)))
-			{
-				this->move_group->execute(start_pose_plan);
-			}
+			this->return_to_initial_pose();
 
 			RCLCPP_INFO(this->get_logger(), "callback fired");
 			if (msg->poses.empty())
@@ -72,7 +85,10 @@ class WaypointFollower : public rclcpp::Node
 			moveit::planning_interface::MoveGroupInterface::Plan first_plan;
 			bool ok = static_cast<bool>(move_group->plan(first_plan));
 			
-			if(!ok) return;
+			if(!ok)
+			{
+				return;
+			} 
 			this->move_group->execute(first_plan);
 			rclcpp::sleep_for(std::chrono::seconds(1));
 
@@ -90,6 +106,7 @@ class WaypointFollower : public rclcpp::Node
 			if (fraction < 0.9)
 			{
 				RCLCPP_WARN(this->get_logger(), "Path planning incomplete, not executing");
+				this->return_to_initial_pose();
 				return;
 			}
 
@@ -98,25 +115,9 @@ class WaypointFollower : public rclcpp::Node
 
 			auto result = this->move_group->execute(plan);
 			if (result != moveit::core::MoveItErrorCode::SUCCESS)
-			{
 				RCLCPP_ERROR(this->get_logger(), "Execution failed");
-			}
 
-			this->move_group->setStartStateToCurrentState();
-			this->move_group->setJointValueTarget(this->initial_joints);
-
-			moveit::planning_interface::MoveGroupInterface::Plan return_plan;
-			ok = static_cast<bool>(this->move_group->plan(return_plan));
-
-			if (ok)
-			{
-				RCLCPP_INFO(this->get_logger(), "Returning to initial state");
-				this->move_group->execute(return_plan);
-			}
-			else
-			{
-				RCLCPP_ERROR(this->get_logger(), "Failed to plan back to initial joint state");
-			}
+			this->return_to_initial_pose();
 		}
 };
 
